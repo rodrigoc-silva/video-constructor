6b3becc55f58fbd2a85efc413565e29ad9b74887
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2021-12-14 16:08:04 -0800

    data structure and text robot

---
+++ user-input/robots/text.js
@@ -0,0 +1,18 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('./credentials/algorithmia.json').apiKey
+
+function robot(content) {
+	fetchContentFromWikipedia(content)
+	// sanitizeContent(content)
+	// breakContentIntoSentences(content)
+	
+	async function fetchContentFromWikipedia(content){
+		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+		const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+		const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+		const wikipediaContent = wikipediaResponse.get()
+		console.log(wikipediaContent)
+	}
+}
+
+module.exports = robot
